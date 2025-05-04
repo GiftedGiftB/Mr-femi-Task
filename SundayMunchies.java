aa6c7069d20d94bf3758ce8de63697dbdc67bd88
Oyakhilome Gift A <asekhamegift@gmail.com>
2025-05-04 09:47:30 -0800

    Add files via upload

---
+++ SundayMunchies.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class SundayMunchies{
+	public static void main(String[] args){
+	Scanner input = new Scanner(System.in);
+
+	// number 1
+
+	/** System.out.print("Enter a number: ");
+	int integer = input.nextInt();
+
+	System.out.print(Function.findMultiple(integer)); **/
+
+ 	// number 2
+
+	
+	// number 5  
+
+	//number 6
+
+	//System.out.print("Enter a number: ");
+	//int integerNumber = input.nextInt();
+
+	//System.out.print(Function.computeSumOfNumber(userNumber));
+
+
+	// number 7
+	System.out.print("Enter a number: ");
+	int leapYear = input.nextInt();
+	System.out.print(Function.isLeapYear(leapYear));
+	
+
+ }
+}
